7aab36fd8df2e7af6b9991004cae63ea4b37f380
Andrew <andrewnzaikombe@gmail.com>
2026-08-14 19:40:01 +0300

    Created DTO

---
+++ backend/src/main/java/com/andrew/hdss/dtos/UserDto.java
@@ -0,0 +1,18 @@
+package com.andrew.hdss.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserDto {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String role;
+    private String email;
+}
