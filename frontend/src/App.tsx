1d995e435cc3dd577ebc7f7cc8009bee18b01b75
Andrew <andrewnzaikombe@gmail.com>
2026-08-14 11:17:58 +0300

    Added routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,20 @@
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import ProtectedRoute from './store/ProtectedRoute'
 
 function App() {
 
   return (
-    <>
-      <p>Paragraph</p>
-    </>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+
+      <Route element={<ProtectedRoute />}>
+        <Route path="/" element={<Home />} />
+      </Route>
+    </Routes>
+
   )
 }
 
