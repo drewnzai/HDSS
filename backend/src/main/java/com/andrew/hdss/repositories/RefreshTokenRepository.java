1224259a95d343c1aad22be98d7012266ce90de1
Andrew <andrewnzaikombe@gmail.com>
2026-08-15 20:18:22 +0300

    Added findByUser method

--- backend/src/main/java/com/andrew/hdss/repositories/RefreshTokenRepository.java
+++ backend/src/main/java/com/andrew/hdss/repositories/RefreshTokenRepository.java
@@ -5,9 +5,12 @@ import com.andrew.hdss.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
     Optional<RefreshToken> findByTokenAndUser(String token, User user);
+
+    List<RefreshToken> findByUser(User user);
 }
