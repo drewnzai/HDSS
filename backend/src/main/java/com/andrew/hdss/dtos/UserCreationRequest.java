d6976e757165ade06d517e456a1b1b19d8555b52
Andrew <andrewnzaikombe@gmail.com>
2026-08-15 19:49:38 +0300

    Added validation for required fields

--- backend/src/main/java/com/andrew/hdss/dtos/UserCreationRequest.java
+++ backend/src/main/java/com/andrew/hdss/dtos/UserCreationRequest.java
@@ -1,18 +1,21 @@
 package com.andrew.hdss.dtos;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class UserCreationRequest {
+    @NotBlank(message = "First Name is required")
     private String firstName;
+    @NotBlank(message = "Last Name is required")
     private String lastName;
+    @NotBlank(message = "Password is required")
     private String password;
+    @NotBlank(message = "Role is required")
     private String role;
+    @NotBlank(message = "Email is required")
     private String email;
 }
