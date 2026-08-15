724162ec49c100c4b3572041032d2f870142f6e8
Andrew <andrewnzaikombe@gmail.com>
2026-08-15 20:17:33 +0300

    Added findByEmail method

--- backend/src/main/java/com/andrew/hdss/repositories/UserRepository.java
+++ backend/src/main/java/com/andrew/hdss/repositories/UserRepository.java
@@ -1,6 +1,7 @@
 package com.andrew.hdss.repositories;
 
 import com.andrew.hdss.models.User;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +12,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
 
     Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
 }
