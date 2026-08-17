294ef5709839d0cac6b161558c1d3fe80d7d3335
Andrew <andrewnzaikombe@gmail.com>
2026-08-17 10:07:07 +0300

    Mark mail sending as an async process

--- backend/src/main/java/com/andrew/hdss/HdssApplication.java
+++ backend/src/main/java/com/andrew/hdss/HdssApplication.java
@@ -3,9 +3,11 @@ package com.andrew.hdss;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.andrew.hdss")
+@EnableAsync
 public class HdssApplication {
 
 	public static void main(String[] args) {
--- backend/src/main/java/com/andrew/hdss/services/MailService.java
+++ backend/src/main/java/com/andrew/hdss/services/MailService.java
@@ -6,6 +6,7 @@ import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
@@ -24,6 +25,7 @@ public class MailService {
         return templateEngine.process("MailTemplate", context);
     }
 
+    @Async
     public void sendMail(NotificationEmail notificationEmail) throws Exception {
         MimeMessagePreparator messagePreparator = mimeMessage -> {
             MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
