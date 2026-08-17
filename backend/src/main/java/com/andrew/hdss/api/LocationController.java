7c51cff533326765655905a2ae765b22b9d06829
Andrew <andrewnzaikombe@gmail.com>
2026-08-17 17:36:57 +0300

    Modify location endpoiny

--- backend/src/main/java/com/andrew/hdss/api/LocationController.java
+++ backend/src/main/java/com/andrew/hdss/api/LocationController.java
@@ -17,7 +17,7 @@ import java.io.IOException;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/locations")
+@RequestMapping("/api/location")
 @RequiredArgsConstructor
 @Tag(name= "Location Management")
 public class LocationController implements LocationApi {
