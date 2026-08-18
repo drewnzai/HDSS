311e355568ef943df755a1e8c70cf6328eb966db
Andrew <andrewnzaikombe@gmail.com>
2026-08-18 16:02:32 +0300

    Created repositories

---
+++ backend/src/main/java/com/andrew/hdss/repositories/IndividualRepository.java
@@ -0,0 +1,9 @@
+package com.andrew.hdss.repositories;
+
+import com.andrew.hdss.models.Individual;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IndividualRepository extends JpaRepository<Individual, Long> {
+}
---
+++ backend/src/main/java/com/andrew/hdss/repositories/MembershipRepository.java
@@ -0,0 +1,9 @@
+package com.andrew.hdss.repositories;
+
+import com.andrew.hdss.models.Membership;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MembershipRepository extends JpaRepository<Membership, Long> {
+}
