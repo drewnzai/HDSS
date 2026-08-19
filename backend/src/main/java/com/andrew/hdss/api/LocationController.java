675607f075d213dac2c59e5f9694eb4ecee8390a
Andrew <andrewnzaikombe@gmail.com>
2026-08-19 16:00:16 +0300

    Updated api path name

--- backend/src/main/java/com/andrew/hdss/api/LocationController.java
+++ backend/src/main/java/com/andrew/hdss/api/LocationController.java
@@ -19,7 +19,7 @@ import java.io.IOException;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/location")
+@RequestMapping("/api/locations")
 @RequiredArgsConstructor
 @Tag(name= "Location Management")
 public class LocationController implements LocationApi {
--- backend/src/main/java/com/andrew/hdss/api/UserController.java
+++ backend/src/main/java/com/andrew/hdss/api/UserController.java
@@ -15,7 +15,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/api/users")
 @AllArgsConstructor
 @Tag(name = "User Management")
 public class UserController implements UserApi {
