45c2ad7a6a8967b0e7fb78426024e00da926cbe2
Andrew <andrewnzaikombe@gmail.com>
2026-09-16 10:04:25 +0300

    Changes expiresAt to String

--- android/app/src/main/java/com/andrew/hdss/dtos/Auth.kt
+++ android/app/src/main/java/com/andrew/hdss/dtos/Auth.kt
@@ -1,18 +1,17 @@
 package com.andrew.hdss.dtos
 
 import kotlinx.serialization.Serializable
-import kotlin.time.ExperimentalTime
-import kotlin.time.Instant
+
 
 @Serializable
 data class LoginRequest(val username: String, val password: String)
 
 @Serializable
-data class LoginResponse @OptIn(ExperimentalTime::class) constructor(
+data class LoginResponse (
     val authenticationToken: String,
     val refreshToken: String,
     val firstName: String,
     val username: String,
     val role: String,
-    val expiresAt: Instant
+    val expiresAt: String
 )
