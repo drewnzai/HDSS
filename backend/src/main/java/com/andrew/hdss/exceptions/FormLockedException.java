29cfb5a201f308a22f5b1abf33bb40a83e603156
Andrew <andrewnzaikombe@gmail.com>
2026-09-20 21:14:32 +0300

    Created Exception

---
+++ backend/src/main/java/com/andrew/hdss/exceptions/FormLockedException.java
@@ -0,0 +1,7 @@
+package com.andrew.hdss.exceptions;
+
+public class FormLockedException extends RuntimeException {
+    public FormLockedException(String message) {
+        super(message);
+    }
+}
