5a52f57713967710db897e5c48dbd3bced7fa1d1
Andrew <andrewnzaikombe@gmail.com>
2026-09-23 11:18:36 +0300

    Created ChoiceController

---
+++ backend/src/main/java/com/andrew/hdss/api/ChoiceController.java
@@ -0,0 +1,45 @@
+package com.andrew.hdss.api;
+
+import com.andrew.hdss.api.swagger.ChoiceApi;
+import com.andrew.hdss.dtos.ChoiceDto;
+import com.andrew.hdss.dtos.CreateChoiceRequest;
+import com.andrew.hdss.dtos.UpdateChoiceRequest;
+import com.andrew.hdss.services.ChoiceService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/choices")
+@RequiredArgsConstructor
+@Tag(name = "Choice Management")
+public class ChoiceController implements ChoiceApi {
+
+    private final ChoiceService choiceService;
+
+    @GetMapping("/list/{listName}")
+    public List<ChoiceDto> getByListName(@PathVariable String listName) {
+        return choiceService.getByListName(listName);
+    }
+
+    @PostMapping
+    public ChoiceDto create(@RequestBody CreateChoiceRequest request) {
+        return choiceService.create(request);
+    }
+
+    @PutMapping("/{id}")
+    public ChoiceDto update(@PathVariable Long id, @RequestBody UpdateChoiceRequest request) {
+        return choiceService.update(id, request);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> delete(@PathVariable Long id) {
+        choiceService.delete(id);
+        return new ResponseEntity<>("Choice deleted successfully",
+                HttpStatus.OK);
+    }
+}
---
+++ backend/src/main/java/com/andrew/hdss/api/swagger/ChoiceApi.java
@@ -0,0 +1,59 @@
+package com.andrew.hdss.api.swagger;
+
+import com.andrew.hdss.dtos.ChoiceDto;
+import com.andrew.hdss.dtos.CreateChoiceRequest;
+import com.andrew.hdss.dtos.UpdateChoiceRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+public interface ChoiceApi {
+    @Operation(summary = "Get Choices by listName forms")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Choices retrieved successfully"
+                    )
+            }
+    )
+    List<ChoiceDto> getByListName(@PathVariable String listName);
+
+    @Operation(summary = "Create a Choice")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Choice created successfully"
+                    )
+            }
+    )
+    ChoiceDto create(@RequestBody CreateChoiceRequest request);
+
+    @Operation(summary = "Update a Choice")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Choice updated successfully"
+                    )
+            }
+    )
+    ChoiceDto update(@PathVariable Long id, @RequestBody UpdateChoiceRequest request);
+
+    @Operation(summary = "Delete a Choice")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Choice deleted successfully"
+                    )
+            }
+    )
+    ResponseEntity<String> delete(@PathVariable Long id);
+}
