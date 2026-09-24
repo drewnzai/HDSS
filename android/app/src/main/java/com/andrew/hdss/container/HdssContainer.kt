56751751a1e76961505c982fa73bd3a61c0fb27d
Andrew <andrewnzaikombe@gmail.com>
2026-09-24 16:31:46 +0300

    Created formDao implementation in AppDatabase; hooked formApiService in app container

--- android/app/src/main/java/com/andrew/hdss/container/HdssContainer.kt
+++ android/app/src/main/java/com/andrew/hdss/container/HdssContainer.kt
@@ -10,6 +10,8 @@ import com.andrew.hdss.network.AuthInterceptor
 import com.andrew.hdss.network.services.LocationApiRepository
 import com.andrew.hdss.network.services.LocationApiService
 import com.andrew.hdss.network.TokenAuthenticator
+import com.andrew.hdss.network.services.FormApiRepository
+import com.andrew.hdss.network.services.FormApiService
 import com.andrew.hdss.network.services.HouseholdApiRepository
 import com.andrew.hdss.network.services.HouseholdApiService
 import com.andrew.hdss.network.services.IndividualApiRepository
@@ -127,6 +129,19 @@ class HdssContainer(private val context: Context) {
             membershipDao = database.membershipDao(),
             householdDao = database.householdDao(),
             individualDao = database.individualDao(),
+            database = database,
+            json = json
+        )
+    }
+
+    private val formApiRepository: FormApiRepository by lazy {
+        apiRetrofit.create(FormApiRepository::class.java)
+    }
+
+    val formApiService: FormApiService by lazy {
+        FormApiService(
+            formApiRepository = formApiRepository,
+            formDao = database.formDao(),
             database = database,
             json = json
         )
--- android/app/src/main/java/com/andrew/hdss/data/AppDatabase.kt
+++ android/app/src/main/java/com/andrew/hdss/data/AppDatabase.kt
@@ -5,21 +5,26 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.andrew.hdss.data.daos.FormDao
 import com.andrew.hdss.data.daos.HouseholdDao
 import com.andrew.hdss.data.daos.IndividualDao
 import com.andrew.hdss.data.daos.LocationDao
 import com.andrew.hdss.data.daos.MembershipDao
+import com.andrew.hdss.data.models.Form
 import com.andrew.hdss.data.models.Household
 import com.andrew.hdss.data.models.Individual
 import com.andrew.hdss.data.models.Membership
 import com.andrew.hdss.data.models.Location
+import com.andrew.hdss.data.models.Visit
 
 @Database(
     entities = [
         Location::class,
         Individual::class,
         Household::class,
-        Membership::class
+        Membership::class,
+        Visit::class,
+        Form::class
     ],
     version = 1,
     exportSchema = true
@@ -31,6 +36,8 @@ abstract class AppDatabase : RoomDatabase() {
     abstract fun individualDao(): IndividualDao
     abstract fun householdDao(): HouseholdDao
     abstract fun membershipDao(): MembershipDao
+
+    abstract fun formDao(): FormDao
 
     companion object {
         private const val DATABASE_NAME = "hdss.db"
