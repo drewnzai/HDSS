823e8dc073cf388155a2e7bffde3b60be7bef24c
Andrew <andrewnzaikombe@gmail.com>
2026-09-24 16:08:02 +0300

    Created FormDao

---
+++ android/app/src/main/java/com/andrew/hdss/data/daos/FormDao.kt
@@ -0,0 +1,20 @@
+package com.andrew.hdss.data.daos
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.andrew.hdss.data.models.Form
+
+@Dao
+interface FormDao {
+
+    @Query("SELECT * FROM forms ORDER BY name")
+    suspend fun getAll(): List<Form>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(form: Form): Long
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(forms: List<Form>)
+}
