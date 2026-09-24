8cfbc92fdd6ca5db43a462274d6ad8cdf9cca6b2
Andrew <andrewnzaikombe@gmail.com>
2026-09-24 15:11:14 +0300

    Visit is only tied to a household

--- backend/src/main/java/com/andrew/hdss/dtos/VisitDto.java
+++ backend/src/main/java/com/andrew/hdss/dtos/VisitDto.java
@@ -10,7 +10,6 @@ public record VisitDto(
         Long id,
         String clientId,
         Long householdId,
-        Long individualId,
         LocalDateTime visitDate,
         VisitStatus status
 ) {
@@ -19,7 +18,6 @@ public record VisitDto(
                 visit.getId(),
                 visit.getClientId(),
                 visit.getHousehold() != null ? visit.getHousehold().getId() : null,
-                visit.getIndividual() != null ? visit.getIndividual().getId() : null,
                 visit.getVisitDate(),
                 visit.getStatus()
         );
