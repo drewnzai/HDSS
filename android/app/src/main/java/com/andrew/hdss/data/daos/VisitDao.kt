25923497554c678111271c1b58ee2fcbd12be6c0
Andrew <andrewnzaikombe@gmail.com>
2026-09-25 15:08:20 +0300

    Keep track of sync status

--- android/app/src/main/java/com/andrew/hdss/data/daos/VisitDao.kt
+++ android/app/src/main/java/com/andrew/hdss/data/daos/VisitDao.kt
@@ -3,6 +3,7 @@ package com.andrew.hdss.data.daos
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import androidx.room.Update
 import com.andrew.hdss.data.models.Visit
 
@@ -15,4 +16,10 @@ interface VisitDao {
 
     @Update
     suspend fun update(visit: Visit)
+
+    @Query("SELECT * FROM visits WHERE synced = 0")
+    suspend fun getUnsynced(): List<Visit>
+
+    @Query("UPDATE visits SET synced = 1 WHERE id = :id")
+    suspend fun markSynced(id: String, serverId: Long)
 }
